3029b43e906eba052bf6b87b78574998501f45a4
Shivam Goel <55030452+shivamgoel008@users.noreply.github.com>
2022-05-04 20:32:05 +0530

    doctor list added

--- app/src/main/java/com/example/peertopeerheathcare/DoctorList.kt
+++ app/src/main/java/com/example/peertopeerheathcare/DoctorList.kt
@@ -1,11 +1,24 @@
 package com.example.peertopeerheathcare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.peertopeerheathcare.Activity.MainActivity
+import kotlinx.android.synthetic.main.activity_doctor_list.*
 
 class DoctorList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_list)
+
+        doc_name.setOnClickListener {
+            val intent = Intent(this@DoctorList, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        doc_name2.setOnClickListener {
+            val intent = Intent(this@DoctorList, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
